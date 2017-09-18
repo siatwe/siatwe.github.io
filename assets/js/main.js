60517da844afb48dc2ce38b3ca0b6618df8d3adc
Simon Weis <siatwe@tutanota.com>
2017-09-18 15:37:49 +0200

    changed: articles format, changed: some articles language

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,28 +4,4 @@ $(document).ready(function () {
             scrollTop: $(".index").offset().top
         }, 600);
     });
-
-    var markInstance = new Mark(document.querySelector(".context"));
-    var keywordInput = document.querySelector("input[name='keyword']");
-    var optionInputs = document.querySelectorAll("input[name='opt[]']");
-
-    function performMark() {
-
-        var keyword = keywordInput.value;
-
-        var options = {};
-        [].forEach.call(optionInputs, function (opt) {
-            options[opt.value] = opt.checked;
-        });
-        markInstance.unmark({
-            done: function () {
-                markInstance.mark(keyword, options);
-            }
-        });
-    }
-
-    keywordInput.addEventListener("input", performMark);
-    for (var i = 0; i < optionInputs.length; i++) {
-        optionInputs[i].addEventListener("change", performMark);
-    }
 });
