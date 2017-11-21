3d068716b1ac08a938f4ab60a3c7396427ee651d
Simon Weis <simonattilaweis@gmail.com>
2017-11-21 10:05:22 +0100

    added: smoothly scroll top > .Content arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    $("i").click(function () {
+        $('html, body').animate({
+            // target class- / id-name
+            scrollTop: $(".Content").offset().top
+        }, 600);
+    });
 });
