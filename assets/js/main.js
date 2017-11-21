b274eb0eb3677e5e94a6c296d31393e1de58726d
Simon Weis <simonattilaweis@gmail.com>
2017-11-21 10:56:03 +0100

    added: .Content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $("i").click(function () {
         $('html, body').animate({
-            // target class- / id-name
             scrollTop: $(".Content").offset().top
         }, 600);
     });
