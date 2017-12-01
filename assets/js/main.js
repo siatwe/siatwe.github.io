c6dcc9095cc7dc0a26fa287078fe935c63bd3e18
Simon Weis <simonattilaweis@gmail.com>
2017-12-01 15:44:50 +0100

    added: particleground

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
             scrollTop: $(".Content").offset().top
         }, 600);
     });
+
+    $('.Body__WallpaperParticles').particleground({
+        dotColor: '#E34C26',
+        lineColor: '#1793d0',
+        particleRadius: 4,
+        density: 8000,
+        proximity: 60
+    });
 });
