2d76801833955f47f17169f4cdc46a2b1aa5e956
Simon Weis <simonattilaweis@tutanota.com>
2019-02-15 23:40:38 +0100

    Deleted: Particle ground

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,4 @@ $(document).ready(function() {
 			scrollTop: $(".Content").offset().top
 		}, 600);
 	});
-
-	$('.Body__WallpaperParticles').particleground({
-		dotColor: 'rgba(227, 76, 38, .7)',
-		lineColor: 'rgba(23, 147, 208, .7)',
-		particleRadius: 4,
-		density: 8000,
-		proximity: 60
-	});
 });
