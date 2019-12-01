da72215c88fa238917de51d55a3f5873bdfdd205
Simon Attila Weis <simonattilaweis@gmail.com>
2019-12-01 10:21:22 +0100

    added some 'nice' style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-	$("i").click(function() {
-		$('html, body').animate({
-			scrollTop: $(".Content").offset().top
-		}, 600);
-	});
+$(document).ready(function () {
+    $('a').on('mouseenter', function () {
+        var color = $(this).data("color");
+        $('.Header__Logo').css('border-color', color);
+    });
+    $('a').on('mouseleave', function () {
+        $('.Header__Logo').css('border-color', 'white');
+    });
 });
